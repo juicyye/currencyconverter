df312d8a3d0d66f33e1d3643bcab0d7ca5b02dbb
param <spsummerdoc@gmail.com>
2024-09-24 18:00:47 +0900

    Chore: mariadb, feignClient 추가

--- src/main/java/hello/currencyconverter/application/user/infrastructure/UserJpaRepository.java
+++ src/main/java/hello/currencyconverter/application/user/infrastructure/UserJpaRepository.java
@@ -1,7 +1,6 @@
 package hello.currencyconverter.application.user.infrastructure;
 
-import hello.currencyconverter.application.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserJpaRepository extends JpaRepository<User, Long> {
+public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
 }
